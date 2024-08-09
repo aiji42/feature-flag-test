bc84abd474906cf4d109889f26e7a16b9590858a
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-08-09 18:06:24 +0900

    update FeatureFlag

--- src/components/FeatureFlag/FeatureFlag.tsx
+++ src/components/FeatureFlag/FeatureFlag.tsx
@@ -2,11 +2,13 @@
 import { FC, ReactNode } from "react";
 import { useFeatureFlag } from "@/hooks/useFeatureFlag";
 
-export const FeatureFlag: FC<{ children: ReactNode; code: string }> = ({
-  code,
-  children,
-}) => {
+export const FeatureFlag: FC<{
+  children: ReactNode | ((active: boolean) => ReactNode);
+  code: string;
+}> = ({ code, children }) => {
   const { active } = useFeatureFlag(code);
+
+  if (typeof children === "function") return children(active);
 
   return active ? <>{children}</> : null;
 };
