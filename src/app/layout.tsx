327eee6dd131c73ac958af3fcea72c0af2caa065
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-08-09 17:30:46 +0900

    update optimize-next tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <NextScript id="optimze-next">
         {`(function(p,r,o,j,e,c,t,g){
-          p['_' + t] = {};g=r.createElement('script');g.src='https://www.googletagmanager.com/gtm.js?id=GTM-'+t;r[o].prepend(g);
-          g=r.createElement('style');g.innerText='.'+e+t+'{visibility:hidden!important}';r[o].prepend(g);
-          r[o][j].add(e+t);setTimeout(function(){if(r[o][j].contains(e+t)){r[o][j].remove(e+t);p['_'+t]=0}},c)
-          })(window,document,'documentElement','classList','loading',2000,'M4V6WSWM')`}
+p['_'+t]={};g=r.createElement('script');g.src='https://www.googletagmanager.com/gtm.js?id=GTM-'+t;r[o].prepend(g);
+g=r.createElement('style');g.innerText='.'+e+t+'{visibility:hidden!important}';r[o].prepend(g);
+r[o][j].add(e+t);setTimeout(function(){if(r[o][j].contains(e+t)){r[o][j].remove(e+t);p['_'+t]=0}},c)
+})(window,document,'documentElement','classList','loading',2000,'TTG682GH')`}
       </NextScript>
       <GoogleAnalytics gaId="G-ZXQTWYEQQQ" />
       <body className={inter.className}>{children}</body>
