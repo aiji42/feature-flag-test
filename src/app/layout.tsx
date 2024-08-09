1757cd788057a63be01a72ad204337b281cfaea3
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-08-09 17:15:42 +0900

    add optimize next tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextScript from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <NextScript id="optimze-next">
+        {`(function(p,r,o,j,e,c,t,g){
+          p['_' + t] = {};g=r.createElement('script');g.src='https://www.googletagmanager.com/gtm.js?id=GTM-'+t;r[o].prepend(g);
+          g=r.createElement('style');g.innerText='.'+e+t+'{visibility:hidden!important}';r[o].prepend(g);
+          r[o][j].add(e+t);setTimeout(function(){if(r[o][j].contains(e+t)){r[o][j].remove(e+t);p['_'+t]=0}},c)
+          })(window,document,'documentElement','classList','loading',2000,'M4V6WSWM')`}
+      </NextScript>
       <body className={inter.className}>{children}</body>
     </html>
   );
